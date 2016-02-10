858acf457b1535046b2f4e6983943eeeb59cfc93
Galen Palmer <palmergs@gmail.com>
2016-02-09 20:51:17 -0500

    picture routing spec; deprecation warning for cors fixed

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,7 @@ module Acs
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    config.middleware.insert_before 0, "Rack::Cors", :debug => true, :logger => (-> { Rails.logger }) do
+    config.middleware.insert_before 0, Rack::Cors, :debug => true, :logger => (-> { Rails.logger }) do
       allow do
         origins '*'
         resource '*',
