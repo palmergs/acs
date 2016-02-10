01582c64990035f74f29824663829d3005369c6a
Galen Palmer <palmergs@gmail.com>
2016-02-09 20:58:00 -0500

    initial picture spec

---
+++ spec/factories/pictures.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :picture do
+    sequence(:name) {|n| "My Picture #{ n }" }
+    description "License or description of this picture"
+    category "testing"
+    sequence(:path) {|n| "pictures/testing/my-picture-#{ n }.png" }
+  end
+
+end
