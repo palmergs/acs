625078c31a529ab6dba5e9546088b21229d5d67e
Galen Palmer <palmergs@gmail.com>
2016-03-02 16:16:07 -0500

    update validation callbacks

--- app/models/magic_spell.rb
+++ app/models/magic_spell.rb
@@ -1,3 +1,6 @@
 class MagicSpell < Thing
 
+  before_validation do
+    self.owner_only = true
+  end
 end
--- app/models/treasure.rb
+++ app/models/treasure.rb
@@ -1,13 +1,12 @@
 class Treasure < Thing
 
   before_validation do
-    self.power = 0
-    self.attack_adj = 0
-    self.breakability = 0
-    self.magic = false
-    self.owner_only = false
-    self.range = 0
-    self.activate_on = ACTIVATE_NONE
-    self.power_cost = 0
+    self.power = 0 unless self.power
+    self.attack_adj = 0 unless self.attack_adj
+    self.breakability = 0 unless self.breakability
+    self.magic = false unless self.magic
+    self.range = 0 unless self.range
+    self.activate_on = ACTIVATE_NONE unless self.activate_on
+    self.power_cost = 0 unless self.power_cost
   end
 end
