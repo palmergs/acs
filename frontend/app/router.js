1317815793fe5e9ea852c8ac9a9a65bf171dfd1b
Galen Palmer <palmergs@gmail.com>
2016-03-20 13:23:16 -0400

    initial routes

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -6,17 +6,33 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('test-picker');
-  this.route('pictures', function() {
-    this.route('edit', { path: '/:id' }, function() {
-
+  this.route('welcome', { path: '/' });
+  this.route('adventures', { path: 'adventures' }, function() {
+    this.route('new', { path: 'new' });
+    this.route('adventure', { path: ':adventure_id' }, function() {
+      this.route('game');
+      this.route('edit');
+      this.route('map');
+      this.route('terrains');
+      this.route('tiles', { path: 'tiles' }, function() {
+        this.route('edit', { path: ':tile_id' });
+      });
+      this.route('creatures', { path: 'creatures'}, function() {
+        this.route('new');
+        this.route('edit', { path: ':creature_id' });
+      });
+      this.route('things', { path: 'things' }, function() {
+        this.route('new');
+        this.route('edit', { path: ':thing_id' });
+      });
+      this.route('regions', { path: 'regions' }, function() {
+        this.route('new');
+        this.route('edit', { path: ':region_id' });
+      });
+      this.route('rooms', { path: 'rooms' }, function() {
+        this.route('edit', { path: ':room_id' });
+      });
     });
-  });
-  this.route('adventures', function() {
-    
-  });
-  this.route('creatures', function() {
-
   });
 });
 
