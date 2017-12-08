4923bbd3ac864735dcb67394fe8ab387d831a98c
Galen Palmer <palmergs@gmail.com>
2017-12-07 20:05:23 -0500

    re-adding cors; rebuild router

--- frontend/app/router.js
+++ frontend/app/router.js
@@ -7,6 +7,43 @@ const Router = EmberRouter.extend({
 });
 
 Router.map(function() {
+  this.route('home', { path: '/home' });
+  this.route('play', { path: '/adventure/:adventure_id' });
+  this.route('adventures', { path: '/adventures' }, function() {
+    this.route('new');
+    this.route('edit', { path: 'edit/:adventure_id' }, function() {
+      this.route('map');
+      this.route('terrains', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:terrain_id' });
+      });
+      this.route('creatures', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:creature_id' });
+      });
+      this.route('things', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:thing_id' });
+      });
+      this.route('tiles', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:tile_id' });
+      });
+      this.route('icons', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:icon_id' });
+      });
+      this.route('regions', function() {
+        this.route('new');
+        this.route('edit', { path: 'edit/:region_id' }, function() {
+          this.route('rooms', function() {
+            this.route('new');
+            this.route('edit', { path: 'edit/:room_id' });
+          });
+        });
+      });
+    });
+  });
 });
 
 export default Router;
