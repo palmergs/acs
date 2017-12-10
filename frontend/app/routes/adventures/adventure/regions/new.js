d550f99abc778ee56f06763d7ef8e1ef7d8ccbbf
Galen Palmer <palmergs@gmail.com>
2017-12-10 08:39:12 -0500

    more wip routes for the edit structure

--- frontend/app/routes/adventures/adventure/regions/new.js
+++ frontend/app/routes/adventures/adventure/regions/new.js
@@ -3,7 +3,9 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     const adventure = this.modelFor('adventures.adventure');
-    return this.store.createRecord('creature', { adventure: adventure });
+    const region = this.store.createRecord('region');
+    region.adventure = adventure;
+    return region;
   },
 
   actions: {
