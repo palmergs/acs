532e71ff24f003d1c900a8f844bb9096b1d300cd
Galen Palmer <palmergs@gmail.com>
2017-12-09 22:03:31 -0500

    CORS supports put and patch

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module Acs
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', :headers => :any, :methods => [:get, :post, :put, :patch, :options]
       end
     end
   end
