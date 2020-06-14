63b231b02808704cbe12941294621bb1ba0cb931
Galen <palmergs@gmail.com>
2020-06-14 13:28:39 -0400

    adding simplecov and prep for devise in spec

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# Acs is the main application module for this rails app
 module Acs
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,8 +4,12 @@ ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
+
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
+
+require 'pundit/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -30,6 +34,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{ ::Rails.root }/spec/fixtures"
@@ -61,4 +66,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # FactoryBot helpers
+  config.include FactoryBot::Syntax::Methods
+
+  # Devise helpers
+  # config.include Devise::Test::ControllerHelpers, type: :controller
 end
