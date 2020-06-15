8097a49cef103d36c68d04b9d9c355c2b85d3885
Galen <palmergs@gmail.com>
2020-06-14 20:29:14 -0400

    added initial map model and trying out the fast_jsonapi serializer

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_231603) do
+ActiveRecord::Schema.define(version: 2020_06_15_001931) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,16 @@ ActiveRecord::Schema.define(version: 2020_06_14_231603) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "maps", force: :cascade do |t|
+    t.bigint "adventure_id", null: false
+    t.string "name", limit: 100, null: false
+    t.text "descr", default: "", null: false
+    t.integer "width", default: 40, null: false
+    t.integer "height", default: 40, null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["adventure_id"], name: "index_maps_on_adventure_id"
+  end
+
+  add_foreign_key "maps", "adventures"
 end
