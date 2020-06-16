1ac6f4260a9772ffa1a7ea88e4d26133cd912f82
Galen <palmergs@gmail.com>
2020-06-16 09:54:36 -0400

    initial take on an api controller; not sure about the relationship

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :adventures, except: %i[new edit]
+    end
+  end
+
   get 'home/welcome'
   get 'home/about'
 
