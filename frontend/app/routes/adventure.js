7ca6043035002aefa1c6ce6df83068099a66c754
Galen <palmergs@gmail.com>
2020-06-18 09:54:47 -0400

    :construction: only query once in index

--- frontend/app/routes/adventure.js
+++ frontend/app/routes/adventure.js
@@ -1,8 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class AdventureRoute extends Route {
-  async model() {
-    console.log("In adventure route...");
-    return this.store.findAll('adventure');
-  }
+
 }
