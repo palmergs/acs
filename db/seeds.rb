ce55ef8eac5754ee627178189f1b0d3b35f9a5d3
Galen Palmer <palmergs@gmail.com>
2020-06-18 19:49:08 -0400

    add seed values

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+sprites = SpriteMap.find_or_create_by(
+    name: 'default', 
+    path: 'default-1280x960.png',
+    width: 1280,
+    height: 960,
+    sprite_width: 64,
+    sprite_height: 96)
+
+adventure = Adventure.find_or_create_by(name: 'Castle on the Hinterlands') do |obj|
+  obj.sprite_map = sprites
+  obj.descr = 'An exciting adventure set in the untamed wilderness of the kingdom.'
+  obj.intro = 'Moneyless and on the run you have fled to the untamed wilderness to try and earn your fortune.'
+end
+
+map = Map.find_or_create_by(name: 'The Hinterlands') do |obj|
+  obj.adventure = adventure
+  obj.descr = 'The area around the castle.'
+  obj.width = 80
+  obj.height = 80
+end
+
+region = Region.find_or_create_by(name: 'The Castle') do |obj|
+  obj.adventure = adventure
+  obj.descr = 'The castle on the hinterlands.'
+  obj.width = 40
+  obj.height = 40
+end
+
