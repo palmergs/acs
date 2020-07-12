adbcdd7a0532aa158114e97bdea67659c2cd882d
Galen <palmergs@gmail.com>
2020-07-12 17:16:06 -0400

    added the tailwind css without too much trouble, base bootstrap still active

--- frontend/ember-cli-build.js
+++ frontend/ember-cli-build.js
@@ -4,9 +4,16 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-    'ember-bootstrap': {
-      'bootstrapVersion': 4,
-      'importBootstrapCSS': false
+//    'ember-bootstrap': {
+//      'bootstrapVersion': 4,
+//      'importBootstrapCSS': false
+//    },
+    'postcssOptions': {
+      compile: {
+        plugins: [
+          { module: require('tailwindcss') }
+        ]
+      }
     }
   });
 
