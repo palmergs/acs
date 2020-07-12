0b981213e7427f48fe687890687b128a75a03759
Galen <palmergs@gmail.com>
2020-07-12 17:16:45 -0400

    prepare for first editable component

--- frontend/app/routes/adventures/index.js
+++ frontend/app/routes/adventures/index.js
@@ -1,10 +1,4 @@
 import Route from '@ember/routing/route';
 
 export default class IndexRoute extends Route {
-  async model() {
-    console.log("In adventure/index route...");
-    let model = this.store.findAll('adventure');
-    console.log(model);
-    return model;
-  }
 }
