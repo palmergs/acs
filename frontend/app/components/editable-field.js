5476e2d2dec839578fd6083ec3218464dab39701
Galen <palmergs@gmail.com>
2020-07-12 17:17:16 -0400

    initial edit component looks kind of ok

---
+++ frontend/app/components/editable-field.js
@@ -0,0 +1,20 @@
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+import { action } from '@ember/object';
+
+export default class EditableFieldComponent extends Component {
+  @tracked isEditing = false
+
+  @action startEditing() {
+    console.log("startEditing: true");
+    this.isEditing = true;
+  }
+
+  @action cancel() {
+    this.isEditing = false;
+  }
+
+  @action submit() {
+    this.isEditing = false;
+  }
+}
