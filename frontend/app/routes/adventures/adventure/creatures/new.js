bf04f66c47348408caa7435505deb2b2d86df530
Galen <palmergs@gmail.com>
2020-07-31 11:34:14 -0400

    :construction: about to move single view routes to top level

--- frontend/app/routes/adventures/adventure/creatures/new.js
+++ frontend/app/routes/adventures/adventure/creatures/new.js
@@ -5,6 +5,18 @@ export default class AdventuresAdventureCreaturesNewRoute extends Route {
     const adventure = this.modelFor('adventures.adventure');
     return this.store.createRecord('creature', {
       adventure: adventure,
+      constitution: 10,
+      strength: 10,
+      wisdom: 8,
+      dexterity: 8,
+      missileSkill: 0,
+      meleeSkill: 20,
+      dodgeSkill: 20,
+      armorSkill: 0,
+      life: 20,
+      power: 10,
+      speed: 6,
+      size: 10,
       magicalDefense: 'none',
       personality: 'Brave',
       outlook: 'Aggressive',
